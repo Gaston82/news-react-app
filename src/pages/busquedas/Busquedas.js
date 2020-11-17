95f081df247bacd397b15cfdc070ce1c2ebad2d3
Gaston82 <cabezaartaza@hotmail.com>
2020-11-18 00:56:11 +0100

    add useState to busquedas

--- src/pages/busquedas/Busquedas.js
+++ src/pages/busquedas/Busquedas.js
@@ -1,31 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "../../components/header/Header";
 
 export const Busquedas = () => {
+  const [search, setsearch] = useState({
+    title: "",
+    from: "",
+    to: "",
+  });
   const handleInputChange = (e) => {
-    console.log("hello");
+    setsearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const { title, from, to } = search;
+
   return (
     <>
       <Header />
-      <h1>Esta es la página de búsquedas</h1>
-      <form>
-        <label>Titulo o contenido</label>
-        <input type="text" onChange={handleInputChange}></input>
-        <label>Desde</label>
-        <input type="date"></input>
-        <label>Hasta</label>
-        <input type="date"></input>
-        <label>Ordenar por...</label>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title or content</label>
+          <input
+            type="text"
+            name="title"
+            onChange={handleInputChange}
+            value={title}
+          ></input>
+        </div>
+        <div>
+          <label>From</label>
+          <input
+            type="date"
+            name="from"
+            value={from}
+            onChange={handleInputChange}
+          ></input>
+        </div>
+        <div>
+          <label>to</label>
+          <input
+            type="date"
+            name="to"
+            value={to}
+            onChange={handleInputChange}
+          ></input>
+        </div>
+
+        <label>Sort by...</label>
         <select>
           <option>relevancia</option>
           <option>popularidad</option>
         </select>
-        <label>Idioma</label>
+        <label>Language</label>
         <select>
-          <option>español</option>
+          <option>spanish</option>
           <option>portugues</option>
-          <option>ingles</option>
+          <option>english</option>
         </select>
       </form>
     </>
