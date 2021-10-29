901f1a20475ac584809a02c9681f329b965f0295
jani1977 <azuladito12@hotmail.com>
2021-10-29 13:56:57 +0200

    Footer files create

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "../../components/header/Header";
 import { NewsList } from "../../components/newsList/NewsList";
+import { Footer } from "../../components/footer/Footer";
 
 export const Home = () => {
   return (
     <>
       <Header />
       <NewsList />
+      <Footer />
     </>
   );
 };
